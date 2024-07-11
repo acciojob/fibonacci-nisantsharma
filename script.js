c95e6be2140c4fd1f47b942fa006618b66e7248d
NISANT SHARMA <106690246+nisantsharma@users.noreply.github.com>
2024-07-11 19:37:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function fibonacci(num) {
+function fibonacci(n) {
 // your code here
+	if (n === 0) return 0;
+    if (n === 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = fibonacci;
